2be8472463587d4011eebf39733d8e148b716046
4kasa786 <149056424+4kasa786@users.noreply.github.com>
2025-08-08 17:48:59 +0530

    Update product.routes.js

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,20 +1,31 @@
 import { Router } from 'express';
 import authorize from '../middlewares/auth.middleware.js';
-import { addProduct, getProductsAdvanced, updateProductQuantity } from '../controllers/product.controller.js';
+import {
+    addProduct,
+    updateProductQuantity,
+    deleteProduct,
+    getProductsAdvanced,
+    getTopProducts,
+    getProducts,
+    getTopKStatus,
+    rebuildTopK
+} from '../controllers/product.controller.js';
 import { validate } from '../middlewares/validate.js';
-import { productSchema } from '../validators/product.validation.js'
-import { updateQuantitySchema } from '../validators/product.validation.js';
-import { getProductsQuerySchema } from '../validators/product.validation.js';
-import { getProducts } from '../controllers/product.controller.js';
+import {
+    productSchema,
+    updateQuantitySchema,
+    getProductsQuerySchema
+} from '../validators/product.validation.js';
 
 const productRouter = Router();
 
 productRouter.post('/', authorize, validate(productSchema), addProduct);
-
 productRouter.put('/:id/quantity', authorize, validate(updateQuantitySchema), updateProductQuantity);
-
+productRouter.delete('/:id', authorize, deleteProduct);
 productRouter.get('/', authorize, getProducts);
+productRouter.get('/advanced', authorize, validate(getProductsQuerySchema), getProductsAdvanced);
+productRouter.get('/top-by-quantity', getTopProducts);
+productRouter.get('/topk/status', getTopKStatus);
+productRouter.post('/topk/rebuild', authorize, rebuildTopK);
 
-productRouter.get('/advanced', authorize, getProductsAdvanced);
-
-export default productRouter;   
+export default productRouter;
